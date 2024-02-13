079d0df0409f36d2ce7d403af75e91731ffe2560
RainbowPigCake <kaitlynwong97@gmail.com>
2024-02-13 18:38:34 -0500

    form validation and navigating between forms

--- lab5/main.js
+++ lab5/main.js
@@ -1,6 +1,32 @@
-function showPayment(e) {
-    e.preventDefault();
-    var div = document.getElementById("payment")
-    div.className = div.className.replace("d-none", "")
+// from https://getbootstrap.com/docs/4.5/components/forms/
+
+'use strict';
+window.addEventListener('load', function () {
+    // $("#payment")[0].classList.remove("d-none") for testing
+
+    var forms = $('.needs-validation');
+    Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            console.log(form.checkValidity())
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            else {
+                $("#payment")[0].classList.remove("d-none")
+                $("#infoForm")[0].classList.add("d-none")
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
+
+function backToScheduling() {
+    $("#payment")[0].classList.add("d-none")
+    $("#infoForm")[0].classList.remove("d-none")
 }
+
+
+
+
 
