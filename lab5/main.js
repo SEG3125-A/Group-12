cb8649929f258cd8f698cb442870d5f563806b85
Obskurity <ctrlaltdeleteyou@gmail.com>
2024-02-13 18:05:35 -0500

    test

--- lab5/main.js
+++ lab5/main.js
@@ -1,6 +1,29 @@
 function showPayment(e) {
     e.preventDefault();
     var div = document.getElementById("payment")
+
+    var custRegex = new RegExp("[a-zA-Z ]+");
+    var emailRegex = new RegExp("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/");
+    var phoneRegex = new RegExp("[0-9]{10}");
+
+    var name = document.getElementById("custName");
+    var email = document.getElementById("custEmail");
+    var phone = document.getElementById("custPhone");
+
+    if(!(custRegex.test(name.value))){
+        name.oninvalid = name.setCustomValidity("Invalid name. Please use letters only. ")
+        console.log("name wrong1")
+        return;
+    }
+    // else if (!emailRegex.test((email.value))){
+    //     console.log("email wrong1")
+    //     return;
+    // }
+    else if (!phoneRegex.test((phone.value))){
+        console.log("phone wrong1")
+        return;
+    }
+    
     div.className = div.className.replace("d-none", "")
 }
 
