acca1283b04a2f6edbcd5b5686857fe04481438f
RainbowPigCake <kaitlynwong97@gmail.com>
2024-02-13 19:02:48 -0500

    date restriction on datetime picker

--- lab5/main.js
+++ lab5/main.js
@@ -7,7 +7,6 @@ window.addEventListener('load', function () {
     var forms = $('.needs-validation');
     Array.prototype.filter.call(forms, function (form) {
         form.addEventListener('submit', function (event) {
-            console.log(form.checkValidity())
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
@@ -20,6 +19,14 @@ window.addEventListener('load', function () {
         }, false);
     });
 }, false);
+
+// datetime picker settings
+var now = new Date();
+var nowDate = now.toISOString().split("T")[0] + "T00:00";
+$("#validationCustom03")[0].setAttribute("min", nowDate);
+now.setFullYear(now.getFullYear() + 1)
+var laterDate = now.toISOString().split("T")[0] + "T00:00";
+$("#validationCustom03")[0].setAttribute("max", laterDate);
 
 function backToScheduling() {
     $("#payment")[0].classList.add("d-none")
