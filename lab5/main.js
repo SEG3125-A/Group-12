0eab5efb0feb651de845aa75b8eb1bda3af4eeca
RainbowPigCake <kaitlynwong97@gmail.com>
2024-02-13 20:12:25 -0500

    add toast

--- lab5/main.js
+++ lab5/main.js
@@ -2,18 +2,25 @@
 
 'use strict';
 window.addEventListener('load', function () {
-    // $("#payment")[0].classList.remove("d-none") for testing
+    //for testing
+    // $("#payment")[0].classList.remove("d-none") 
+    // $("#paymentToast").toast('show')
+
+
 
     var forms = $('.needs-validation');
     Array.prototype.filter.call(forms, function (form) {
         form.addEventListener('submit', function (event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            else {
+            event.preventDefault();
+            event.stopPropagation();
+            if (form.checkValidity()) {
+                // if ($("#payment")[0)
                 $("#payment")[0].classList.remove("d-none")
                 $("#infoForm")[0].classList.add("d-none")
+                if (form.id === 'paymentForm') {
+                    $("#toastMessage").text(`Hello ${$("#validationCustom04").val()}! Your booking for a ${$("#validationCustom01").val()}cut with ${$("#validationCustom02").val()} has been successfully booked for ${new Date($("#validationCustom03").val()).toLocaleTimeString()} on ${new Date($("#validationCustom03").val()).toLocaleDateString()}`)
+                    $("#paymentToast").toast('show')
+                }
             }
             form.classList.add('was-validated');
         }, false);
@@ -22,7 +29,7 @@ window.addEventListener('load', function () {
 
 // datetime picker settings
 var now = new Date();
-now.setMinutes(now.getMinutes - now.getTimezoneOffset())
+now.setMinutes(now.getMinutes() - now.getTimezoneOffset())
 var nowDate = now.toISOString().split("T")[0] + "T00:00";
 $("#validationCustom03")[0].setAttribute("min", nowDate);
 
@@ -37,5 +44,9 @@ function backToScheduling() {
 
 
 
+function onSubmitPayment(e) {
+    $("#toastMessage").text(`Hello ${$("#validationCustom04").val()}! Your booking for a ${$("#validationCustom01").val()}cut with ${$("#validationCustom02").val()} has been successfully booked for ${new Date($("#validationCustom03").val()).toLocaleTimeString()} on ${new Date($("#validationCustom03").val()).toLocaleDateString()}`)
+    $("#paymentToast").toast('show')
+}
 
 
