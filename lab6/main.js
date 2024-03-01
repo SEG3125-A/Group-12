43ff0050a3717fd10c724e7e6c72414be3e9a433
RainbowPigCake <kaitlynwong97@gmail.com>
2024-02-29 19:20:58 -0500

    add dummy values

---
+++ lab6/main.js
@@ -0,0 +1,13 @@
+// const form = $("#myform")
+// form.on("submit", submitForm(e))
+
+
+
+function submitForm(e) {
+    // e.preventDefault()
+    const form = $("#myform")
+    // console.log(form.serializeArray())
+    // form.serializeArray().forEach(element => {
+    //     console.log(element)
+    // });
+}
