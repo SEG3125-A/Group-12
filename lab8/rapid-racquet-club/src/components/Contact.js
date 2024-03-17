ffd2aa03365905b9e9b6844add372c937ba1a8c1
RainbowPigCake <kaitlynwong97@gmail.com>
2024-03-17 16:05:42 -0400

    fix spacing and submit button

--- lab8/rapid-racquet-club/src/components/Contact.js
+++ lab8/rapid-racquet-club/src/components/Contact.js
@@ -41,7 +41,7 @@ export default function Contact() {
             </div>
 
             <div className='row container d-flex my-4'>
-                <div className='col-md-8 offset-md-3'>
+                <div className='col-md-7 offset-md-3'>
                     <form onSubmit={handleSubmit}>
                         <div className="form-group row mb-4 tertiary-bg p-4 border" style={{ borderRadius: '24px' }}>
                             <label htmlFor="name" className="col-sm-2 col-form-label primary fw-semibold" style={{ fontFamily: 'Inter, serif', fontSize: '20px' }}>Name</label>
@@ -55,14 +55,14 @@ export default function Contact() {
                             <label htmlFor="phone" className="col-sm-2 col-form-label primary fw-semibold" style={{ fontFamily: 'Inter, serif', fontSize: '20px' }}>Phone</label>
                             <input type="tel" className="col-sm-2 form-control form-control-sm rounded-pill" id="phone" name="phone" value={formData.phone} onChange={handleChange} style={{ width: '70%' }} />
                         </div>
-                        <div className="form-group row mb-4 tertiary-bg p-4 border" style={{ borderRadius: '24px' }}>
+                        <div className="form-group row tertiary-bg p-4 border" style={{ borderRadius: '24px' }}>
                             <label htmlFor="inquiry" className="col-sm-2 col-form-label primary fw-semibold" style={{ fontFamily: 'Inter, serif', fontSize: '20px' }}>Inquiry</label>
                             <textarea className="col-sm-2 form-control form-control-sm" id="inquiry" name="inquiry" rows="4" value={formData.inquiry} onChange={handleChange} style={{ width: '70%', borderRadius: '24px' }}></textarea>
                         </div>
-                        <Button style={{ borderRadius: "8px", width: '347px', height: '48px', marginTop: '100px' }} className="mx-3 fw-semibold secondary-bg border-0">Submit</Button>
+                        <Button type='submit' style={{ borderRadius: "8px", width: '347px', height: '48px', marginTop: '40px', marginBottom: '100px' }} className="mx-3 fw-semibold secondary-bg border-0">Submit</Button>
                     </form>
                 </div>
-                <div className='col-md-1'>
+                <div className='col-md-2'>
                     <div className='tertiary-bg border' style={{ width: '399px', height: '312px', fontFamily: 'Inter, serif', fontSize: '20px', borderRadius: '24px' }}>
                         <p style={{ margin: '24px' }}>
                             <b>Location</b><br />123 Location St Province, Canada ABC 123 <b><br /><br />Phone Number</b><br />(123) 456-7890<br /><br /><b>Email</b><br />contact@rrr.ca
