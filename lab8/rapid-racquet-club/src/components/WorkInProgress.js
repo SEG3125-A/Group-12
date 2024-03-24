aa299208b8e3efcff21e2bfc628a7de1a92c7788
RainbowPigCake <kaitlynwong97@gmail.com>
2024-03-23 22:03:23 -0400

    translate work in progress

--- lab8/rapid-racquet-club/src/components/WorkInProgress.js
+++ lab8/rapid-racquet-club/src/components/WorkInProgress.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PageHeader from './PageHeader'
+import { useTranslation } from 'react-i18next'
+
 
 export default function WorkInProgress() {
+    const { t } = useTranslation()
+
     return (
-        <PageHeader text={'Work in Progress'} />
+        <PageHeader text={t('wip')} />
 
     )
 }
