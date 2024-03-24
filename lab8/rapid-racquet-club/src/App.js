93670705da86a432678635fb4379c0b8f4866d38
Obskurity <ctrlaltdeleteyou@gmail.com>
2024-03-23 20:20:19 -0400

    fixed lang toggler broken

--- lab8/rapid-racquet-club/src/App.js
+++ lab8/rapid-racquet-club/src/App.js
@@ -15,6 +15,15 @@ import { initReactI18next } from 'react-i18next';
 import translationsEn from './translations/en.json';
 import translationsFr from './translations/fr.json';
 
+i18n.use(initReactI18next).init({
+  resources: {
+      en: { translation: translationsEn },
+      fr: { translation: translationsFr }
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false }
+})
 
 function App() {
   const [language, setLanguage] = useState(localStorage.getItem('language') || 'en');
